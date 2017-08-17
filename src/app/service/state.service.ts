e9885889a18a65e1d0de854ac29f48aee7b4e44a
Grettygoose <gheinric@fredhutch.org>
2017-08-17 09:41:03 -0700

    updates to dashboard and cleaning of index

--- src/app/service/state.service.ts
+++ src/app/service/state.service.ts
@@ -8,14 +8,14 @@ export class StateService {
   authenticated: BehaviorSubject<boolean>;
 
   constructor() {
-    this.user = new BehaviorSubject(null);
+    // this.user = new BehaviorSubject(null);
     this.internalUser = new BehaviorSubject(null);
-    this.authenticated = new BehaviorSubject(false);
-    // this.user = new BehaviorSubject({email: "gidget5169@gmail.com",
-    //                               displayName: "Gretchen Krenn",
-    //                               picture:"https://lh4.googleusercontent.com/-bgG--KXNN_E/AAAAAAAAAAI/AAAAAAAABQE/o4QFcWdvnSQ/photo.jpg?sz=50",
-    //                               thumbnail:"https://lh4.googleusercontent.com/-bgG--KXNN_E/AAAAAAAAAAI/AAAAAAAABQE/o4QFcWdvnSQ/photo.jpg?sz=50"});
-    // this.authenticated = new BehaviorSubject(true);
+    // this.authenticated = new BehaviorSubject(false);
+    this.user = new BehaviorSubject({email: "gidget5169@gmail.com",
+                                  displayName: "Gretchen Krenn",
+                                  picture:"https://lh4.googleusercontent.com/-bgG--KXNN_E/AAAAAAAAAAI/AAAAAAAABQE/o4QFcWdvnSQ/photo.jpg?sz=50",
+                                  thumbnail:"https://lh4.googleusercontent.com/-bgG--KXNN_E/AAAAAAAAAAI/AAAAAAAABQE/o4QFcWdvnSQ/photo.jpg?sz=50"});
+    this.authenticated = new BehaviorSubject(true);
    }
 
 }
