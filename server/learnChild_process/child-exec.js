d9d82cf8f88dab394819c54f3a7e8a5cdb5a6265
Jenny Zhang <canaantt@gmail.com>
2017-10-19 13:23:41 -0700

    deleted server

--- server/learnChild_process/child-exec.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require('child_process');
-
-exec('find . -type f | wc -l', (err, stdout, stderr) => {
-  if (err) {
-     console.error(`exec error: ${err}`);
-     return;
-  }
-  console.log(`Number of files ${stdout}`);
-});
--- server/learnChild_process/child.js
+++
@@ -1,9 +0,0 @@
-process.on('message', (msg) => {
-  console.log('Message from parent:', msg);
-});
-
-let counter = 0;
-
-setInterval(() => {
-   process.send({counter: counter++});
-}, 1000);
