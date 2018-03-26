51cc17ce2580da3e3a4e6ce37875ff89e4fb4a61
Jenny Zhang <canaantt@gmail.com>
2018-03-25 21:26:36 -0700

    set up upload kubernetes cluster; client now in s3/cloudfront; still testing POST

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://dev.oncoscape.sttrcancer.io/api/',
-  oAuthRedirectUri: 'https://dev.oncoscape.sttrcancer.io/upload/index.html'
+  // apiBaseUrl: 'https://dev.oncoscape.sttrcancer.io/api/',
+  apiBaseUrl: 'http://uploading.os-user-data.jennylou.info/api/',
+  oAuthRedirectUri: 'http://os-user-data.jennylou.info.s3-website-us-west-2.amazonaws.com/index.html'
 };
