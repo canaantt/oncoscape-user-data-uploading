7bdc1a0233b7b8150de96f638cf1762a35732c4b
Jenny Zhang <canaantt@gmail.com>
2018-03-27 12:23:11 -0700

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // apiBaseUrl: 'https://dev.oncoscape.sttrcancer.io/api/',
   // apiBaseUrl: 'http://uploading.os-user-data.jennylou.info/api/',
-  apiBaseUrl: 'http://uploading.os-user-data.jennylou.info/',
+  apiBaseUrl: 'http://upload-server.os.sttrcancer.io/',
   oAuthRedirectUri: 'http://os-user-data.jennylou.info.s3-website-us-west-2.amazonaws.com/index.html'
 };
