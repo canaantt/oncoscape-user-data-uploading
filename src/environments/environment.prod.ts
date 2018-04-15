75c0aff8f76b24c2c39578d89ccb05a67108dc18
Jenny Zhang <canaantt@gmail.com>
2018-04-14 17:50:09 -0700

    change BaseAPI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://upload-server.os.sttrcancer.io/',
+  apiBaseUrl: 'https://upload-server.os.sttrcancer.io/api/',
   oAuthRedirectUri: 'http://user-data.os.sttrcancer.io.s3-website-us-west-2.amazonaws.com/index.html'
 };
